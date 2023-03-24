66e3645e8cefed048f8a71c8f7bbf73429262c05
Vl-pod <podsevalow@gmail.com>
2023-03-24 23:04:37 +0400

    кнопка выхода готова

---
+++ public/homePage.js
@@ -0,0 +1,11 @@
+const logoutButton = new LogoutButton();
+
+logoutButton.action = () => {
+	const cb = (response) => {
+		if (response.success) {
+			location.reload();
+		}
+	}
+
+	ApiConnector.logout(cb);
+}
